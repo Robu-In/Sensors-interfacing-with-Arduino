b299eb4b36f3841a9137671175aaae9f47e7af9d
Robu - Electronics Store <158466906+Robu-In@users.noreply.github.com>
2024-06-11 18:49:29 +0530

    Add files via upload

---
+++ Vibration_sensor_code.ino
@@ -0,0 +1,25 @@
+int led = 13;
+int vs =9; // vibration sensor
+
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(vs, INPUT); 
+  Serial.begin(9600); 
+
+}
+void loop(){
+  long measurement =vibration();
+  delay(50);
+  Serial.println(measurement);
+  if (measurement > 50){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW); 
+  }
+}
+
+long vibration(){
+  long measurement=pulseIn (vs, HIGH);  //wait for the pin to get HIGH and returns measurement
+  return measurement;
+}
